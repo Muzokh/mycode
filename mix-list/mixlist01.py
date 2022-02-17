dee0085bb991bb3d6716130d8b4b3c3ad1c86ef9
muzokh <muzokh@hotmail.com>
2022-02-16 23:31:31 -0700

    practice2

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 my_list =   ["192.168.0.5", 5060, "UP"]
-print("1st item in list",my_list[0])
+print("1st item in list",my_list[0],my_list[1])
 print("1st item in list",my_list[1])
 print("2nd item in list"+str(my_list[1]))
